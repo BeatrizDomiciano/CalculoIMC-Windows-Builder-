5f2d99260890b0bb65951c114dcc548e248fc4f1
beatr <beatriz.domiciano.santos@gmail.com>
2021-08-19 22:06:08 -0300

    Adicionando novo arquivo

---
+++ ExerciciosJava02.08/src/exerciciosJava0208/exercicio04.java
@@ -0,0 +1,22 @@
+package exerciciosJava0208;
+import javax.swing.JOptionPane;
+public class exercicio04 {
+
+	public static void main(String[] args) {
+/*programa que leia as quatro medidas hp, lp, ha e la, calcule e imprima quanto 
+*azulejos com as medidas dadas são necessários para cobrir a parede dada.*/
+
+		double medidas[]= new double [4];
+		double quantAzu = 0;
+		
+		medidas[0]= Integer.parseInt(JOptionPane.showInputDialog("Qual a altura da parede?"));
+		medidas[1]= Integer.parseInt(JOptionPane.showInputDialog("Qual a largura da parede?"));
+		medidas[2]= Integer.parseInt(JOptionPane.showInputDialog("Qual a altura do azulejo?"));
+		medidas[3]= Integer.parseInt(JOptionPane.showInputDialog("Qual a largura do azulejo?"));
+		
+		quantAzu = (medidas[0] * medidas[1]) / ((medidas[2]/100) * (medidas[3]/100) );
+		
+		JOptionPane.showMessageDialog(null,"A quantidade de azulejos necessaria é: " + String.format("%.2f",quantAzu));	
+	}
+
+}
---
+++ TabuadaJava/src/tabuadaJava.java
@@ -0,0 +1,8 @@
+public class tabuadaJava {
+import javax.swing.JOptionPane;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
